914d5df39c75bf9ca14aed674e4da50be3356257
Sean Takahashi <seanyuto@gmail.com>
2025-03-08 16:17:38 -0500

    created assignment python file

---
+++ sorting.py
@@ -0,0 +1,13 @@
+## Lab03- Sorting ##
+
+
+# In this lab, you will be writing three different sorting methods. 
+# The three algorithms are selection sort, a bubble sort, and a insertion sort.
+# Although there are not good algorithms to sort a list, they serve as a good educational purpose to understand list structure.
+
+# Bubble Sort
+
+# This is the most basic sorting method. 
+# From the left side, if the current element is greater than the next element, swap them. 
+# If not, leave as is. 
+# Continue this process until the list is sorted.
