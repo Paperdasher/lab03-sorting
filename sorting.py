145eb63830d6770954d2af70d167f701cf335b67
Sean Takahashi <seanyuto@gmail.com>
2025-03-09 00:38:57 -0500

    added each function

--- sorting.py
+++ sorting.py
@@ -1,13 +1,29 @@
-## Lab03- Sorting ##
+# Lab03- Sorting 
 
 
 # In this lab, you will be writing three different sorting methods. 
 # The three algorithms are selection sort, a bubble sort, and a insertion sort.
 # Although there are not good algorithms to sort a list, they serve as a good educational purpose to understand list structure.
 
-# Bubble Sort
 
-# This is the most basic sorting method. 
-# From the left side, if the current element is greater than the next element, swap them. 
-# If not, leave as is. 
-# Continue this process until the list is sorted.
+# Read the README on the repo to understand each algorithm. 
+# Understanding the algorithm should also allow you to think about the time complexity.
+
+
+def bubblesort(data):
+    # Your code here #
+    return data
+
+
+
+
+def selectionsort(data):
+    # Your code here #
+    return data
+
+
+
+def insertionsort(data):
+    # Your code here #
+    return data
+
